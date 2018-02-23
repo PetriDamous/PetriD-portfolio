f5473fe537e1ca1011e4841fc2daf8746b054cc2
dwilliams64 <dwilliams_64@yahoo.com>
2018-02-23 20:34:58 +0000

    Implemented validations for portfolios, blog, and skills

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,2 +1,6 @@
 class Portfolio < ApplicationRecord
+    #validates :title, presence: true, length: {minimum: 3}
+    #validates :subtitle, presence: true, length: {minimum: 3}
+    #validates_presence_of :body, :main_image, :thumb_image
+    validates_presence_of :title, :body, :subtitle
 end
