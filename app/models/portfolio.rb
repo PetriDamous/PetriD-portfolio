1d33e1195d53f3311d70c0d8a439ce91088824ff
dwilliams64 <dwilliams_64@yahoo.com>
2018-02-24 22:19:00 +0000

    Implemented custom scopes

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -2,5 +2,13 @@ class Portfolio < ApplicationRecord
     #validates :title, presence: true, length: {minimum: 3}
     #validates :subtitle, presence: true, length: {minimum: 3}
     #validates_presence_of :body, :main_image, :thumb_image
-    validates_presence_of :title, :body
+    #validates_presence_of :title, :body
+    
+    #Custom scope method way
+    def self.javascript 
+        where(subtitle: "Javascript")
+    end
+    
+    #Custom scope lamda way
+    scope :ruby_rails, -> {where(subtitle: "Ruby on Rails")}
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,13 @@
 #
 # Examples:
 #
+3.times do |topic|
+    Topic.create!(
+        title: "Topic #{topic}"
+        )
+end
+
+puts "3 topics created"
 
 10.times do |blog|
     Blog.create!(
@@ -11,7 +18,9 @@
                 sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
                 Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
                 Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        topic_id: Topic.last.id        
+                
     )
 end
 
@@ -27,10 +36,10 @@ end
 
 puts "5 skills created"
 
-9.times do |portfolio_item|
+8.times do |portfolio_item|
     Portfolio.create!(
         title: "Portofolio title #{portfolio_item}",
-        subtitle: "My great service",
+        subtitle: "Ruby on Rails",
         body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
                 Pellentesque pulvinar pellentesque habitant morbi. Mi proin sed libero enim sed. 
                 Nunc sed blandit libero volutpat sed. Nullam non nisi est sit amet.",
@@ -40,6 +49,18 @@ puts "5 skills created"
         )
 end
 
+1.times do |portfolio_item|
+    Portfolio.create!(
+        title: "Portofolio title #{portfolio_item}",
+        subtitle: "Javascript",
+        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                Pellentesque pulvinar pellentesque habitant morbi. Mi proin sed libero enim sed. 
+                Nunc sed blandit libero volutpat sed. Nullam non nisi est sit amet.",
+        main_image: "http://via.placeholder.com/600x400",       
+        thumb_image: "http://via.placeholder.com/350x200",
+        
+        )
+end
 
 puts "9 portfolio items created"
     
