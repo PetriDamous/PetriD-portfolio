d1e8a852d2ad8397c9e5adf6c41b3aac3fa3d2b3
dwilliams64 <dwilliams_64@yahoo.com>
2018-02-24 20:33:24 +0000

    Integrated database relationship between topics and blog posts

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -2,5 +2,5 @@ class Portfolio < ApplicationRecord
     #validates :title, presence: true, length: {minimum: 3}
     #validates :subtitle, presence: true, length: {minimum: 3}
     #validates_presence_of :body, :main_image, :thumb_image
-    validates_presence_of :title, :body, :subtitle
+    validates_presence_of :title, :body
 end
