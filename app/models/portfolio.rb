c06ed9a6f091d4c9497af8223a63c92a0e585c4d
dwilliams64 <dwilliams_64@yahoo.com>
2018-02-25 21:05:13 +0000

    Implemented image defaults for portfolio items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,7 +1,7 @@
 class Portfolio < ApplicationRecord
     #validates :title, presence: true, length: {minimum: 3}
     #validates :subtitle, presence: true, length: {minimum: 3}
-    #validates_presence_of :body, :main_image, :thumb_image
+    validates_presence_of :title, :body, :main_image, :thumb_image
     #validates_presence_of :title, :body
     
     #Custom scope method way
@@ -11,4 +11,12 @@ class Portfolio < ApplicationRecord
     
     #Custom scope lamda way
     scope :ruby_rails, -> {where(subtitle: "Ruby on Rails")}
+    
+    #Sets defaults for images if there isn't any
+    after_initialize :set_default
+    
+    def set_default
+        self.main_image ||= "http://via.placeholder.com/600x400"
+        self.thumb_image ||= "http://via.placeholder.com/350x200"
+    end    
 end
