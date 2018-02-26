aced354b22804d124d5af4ca56e809ff3652eddb
dwilliams64 <dwilliams_64@yahoo.com>
2018-02-26 14:34:05 +0000

    Integrated nested attributes for portfolio model

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,6 +1,10 @@
 class Portfolio < ApplicationRecord
     include Placeholder
     has_many :technologies
+    
+    accepts_nested_attributes_for :technologies, 
+                                   reject_if: lambda {|attr| attr['name'].blank?}
+    
     #validates :title, presence: true, length: {minimum: 3}
     #validates :subtitle, presence: true, length: {minimum: 3}
     validates_presence_of :title, :body, :main_image, :thumb_image
