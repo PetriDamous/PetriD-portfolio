1ccddc70918b9fdb8001f96e0927eeea90d725da
dwilliams64 <dwilliams_64@yahoo.com>
2018-04-17 15:33:09 +0000

    Impleemented refactor for navigation and active statuses

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 module ApplicationHelper
     def login_helper(style = "")
-        str = "<span class='#{style}'> /</span>"
+        #str = "<span class='#{style}'> /</span>"
         if current_user.is_a?(GuestUser)
-            (link_to "Login", new_user_session_path, class: style) + (str).html_safe + 
+            (link_to "Login", new_user_session_path, class: style) + 
             " ".html_safe +
             (link_to "Register", new_user_registration_path, class: style)
         else 
@@ -16,11 +16,54 @@ module ApplicationHelper
             content_tag(:p, greeting, class: "source-gretting")
         end
     end
+
+    def nav_items
+        [
+            {url: root_path, title: 'Home'}, 
+            
+            {url: about_me_path, title: 'About'}, 
+            
+            {url: contact_path, title: 'Contact'}, 
+            
+            {url: blogs_path, title: 'Blog'}, 
+            
+            {url: portfolios_path, title: 'Portfolio'}
+            
+            ]
+    end
+    
+ 
+    def nav_helper(style , tag_type, nav_lambda)
+        nav_links = ''
+        
+        nav_items.each { |item| nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style} #{active?(item[:url])}'>#{item[:title]}</a></#{tag_type}>" }
+
+        nav_items.html_safe    
+    end
+    
+    def active?(path)
+        "active" if current_page? path    
+    end
+    
     
     #For custom Petrid gem fix it later
     #def copyright_generator
         #PetridViewTool::Renderer.copyright("Dimitri Williams", "All rights reserved")
     #end
+    
+=begin  old nav_helper before refactor 
+    def nav_helper(style , tag_type)
+nav_links = <<NAV
+<#{tag_type}><a href="#{root_path}" class="#{style} #{active?(root_path)}">Home</a></#{tag_type}>
+<#{tag_type}><a href="#{about_me_path}" class="#{style} #{active?(about_me_path)}">About</a></#tag_type}>
+<#{tag_type}><a href="#{contact_path}" class="#{style} #{active?(contact_path)}">Contact</a></#tag_type}>
+<#{tag_type}><a href="#{blogs_path}" class="#{style} #{active?(blogs_path)}">Blog</a></#tag_type}>
+<#{tag_type}><a href="#{portfolios_path}" class="#{style} #{active?(portfolios_path)}">Portfolio</a></#tag_type}>
+NAV
+    nav_links.html_safe    
+    end
+=end 
+    
     
     
 end
