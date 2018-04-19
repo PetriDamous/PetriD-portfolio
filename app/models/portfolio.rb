de9637424521df8e80efc1e78ed3c7e41f2bb4a8
dwilliams64 <dwilliams_64@yahoo.com>
2018-04-19 15:04:22 +0000

    Updated portfolio to include attr for position

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -24,5 +24,9 @@ class Portfolio < ApplicationRecord
     def set_default
         self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
         self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
-    end    
+    end
+    
+    def self.by_position
+        order("position ASC")
+    end
 end
